820dadbf6f4df9c36cf8bf2e5615218516f999dd
DavyVan <affqqa@163.com>
2021-03-04 15:34:52 -0800

    backup before reinstalling OS

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import MatrixTest
 
-def parser(str):
-    pass
+
+def parser(stdout: str) -> str:
+    lines = stdout.splitlines()
+    print(lines)
+    return "hello"
+
 
 def main():
-    mt = MatrixTest.MatrixTest("{}{}", {"aaa": ["a", "b"]}, parser)
+    mt = MatrixTest.MatrixTest("dir F:\\坚果云\\CRPG-Book-Chinese\\{path}", {"path": ["宣传材料", "归档"]}, parser)
+    mt.run()
+    mt.run(3)
 
 
 if __name__ == '__main__':
